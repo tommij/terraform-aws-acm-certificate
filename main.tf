cc736b41b471dfdcddf85e410deeff0902629268
bernard <bernard@traveloka.com>
2018-04-04 13:17:30 +0700

    update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_acm_certificate" "certificate" {
+resource "aws_acm_certificate" "this" {
   domain_name       = "${var.domain_name}"
   validation_method = "${var.validation_method}"
 
@@ -7,25 +7,26 @@ resource "aws_acm_certificate" "certificate" {
     ProductDomain = "${var.product_domain}"
     Environment   = "${var.environment}"
     Description   = "${var.description}"
+    ManagedBy     = "Terraform"
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
-  name    = "${aws_acm_certificate.certificate.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.certificate.domain_validation_options.0.resource_record_type}"
+resource "aws_route53_record" "this" {
+  name    = "${aws_acm_certificate.this.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.this.domain_validation_options.0.resource_record_type}"
   zone_id = "${data.aws_route53_zone.zone.id}"
-  records = ["${aws_acm_certificate.certificate.domain_validation_options.0.resource_record_value}"]
+  records = ["${aws_acm_certificate.this.domain_validation_options.0.resource_record_value}"]
   ttl     = 60
   count   = "${var.validation_method == "DNS" ? 1 : 0}"
 }
 
 resource "aws_acm_certificate_validation" "dns_validation" {
-  certificate_arn         = "${aws_acm_certificate.certificate.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+  certificate_arn         = "${aws_acm_certificate.this.arn}"
+  validation_record_fqdns = ["${aws_route53_record.this.fqdn}"]
   count                   = "${var.validation_method == "DNS" ? 1 : 0}"
 }
 
 resource "aws_acm_certificate_validation" "email_validation" {
-  certificate_arn = "${aws_acm_certificate.certificate.arn}"
+  certificate_arn = "${aws_acm_certificate.this.arn}"
   count           = "${var.validation_method == "EMAIL" ? 1 : 0}"
 }
